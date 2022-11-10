142275ee330621181879efcaebd29d528513706f
cuongnbph22662 <95554152+cuongnbph22662@users.noreply.github.com>
2022-11-10 10:44:39 +0700

    10/11/2022_HungVH
    10/11/2022_HungVH

--- app/src/main/java/cuongnbph22662/poly/duansotaydulich/MainActivity.java
+++ app/src/main/java/cuongnbph22662/poly/duansotaydulich/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Hưng sad boy
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
