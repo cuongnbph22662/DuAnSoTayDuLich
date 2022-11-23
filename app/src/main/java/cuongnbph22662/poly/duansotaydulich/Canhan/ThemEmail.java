7e7a4d9d52cc9528fb82016868560117bd3d31e6
hungvhph22652 <117882306+hungvhph22652@users.noreply.github.com>
2022-11-23 10:49:50 +0700

    Lag

---
+++ app/src/main/java/cuongnbph22662/poly/duansotaydulich/Canhan/ThemEmail.java
@@ -0,0 +1,16 @@
+package cuongnbph22662.poly.duansotaydulich.Canhan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cuongnbph22662.poly.duansotaydulich.R;
+
+public class ThemEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_them_email);
+    }
+}
