614600d8f6d6bd20b0d837dea3af8ff86d503bcb
hungvhph22652 <117882306+hungvhph22652@users.noreply.github.com>
2022-11-24 11:28:36 +0700

    24/11

--- app/src/main/java/cuongnbph22662/poly/duansotaydulich/Canhan/DangNhap.java
+++ app/src/main/java/cuongnbph22662/poly/duansotaydulich/Canhan/DangNhap.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import cuongnbph22662.poly.duansotaydulich.R;
 
-public class DoiMK extends AppCompatActivity {
+public class DangNhap extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_doi_mk);
+        setContentView(R.layout.activity_dang_nhap);
     }
 }
--- app/src/main/java/cuongnbph22662/poly/duansotaydulich/fragment/CaNhanFragment.java
+++ app/src/main/java/cuongnbph22662/poly/duansotaydulich/fragment/CaNhanFragment.java
@@ -1,22 +1,26 @@
 package cuongnbph22662.poly.duansotaydulich.fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import cuongnbph22662.poly.duansotaydulich.Canhan.CreateAccount;
+import cuongnbph22662.poly.duansotaydulich.Canhan.DangNhap;
+import cuongnbph22662.poly.duansotaydulich.Canhan.TTCaNhan;
 import cuongnbph22662.poly.duansotaydulich.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CaNhanFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CaNhanFragment extends Fragment {
-
+    TextView btnDangNhap,btnTaoTaiKhoan,btnTTTaiKhoan;
+    Context context;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,25 @@ public class CaNhanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ca_nhan, container, false);
+        View view = inflater.inflate(R.layout.fragment_ca_nhan, container, false);
+        btnDangNhap = view.findViewById(R.id.btnDangNhap);
+        btnTaoTaiKhoan = view.findViewById(R.id.btnTaoTaiKhoan);
+        btnTTTaiKhoan = view.findViewById(R.id.btnTTTaiKhoan);
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),DangNhap.class);
+                startActivity(intent);
+
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
