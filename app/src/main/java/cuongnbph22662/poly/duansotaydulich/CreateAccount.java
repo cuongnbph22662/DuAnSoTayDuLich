f18746d7f23e88ef9e5f5864d87d08d1d0adce60
hungvhph22652 <117882306+hungvhph22652@users.noreply.github.com>
2022-11-28 12:54:28 +0700

    ABC

--- app/src/main/java/cuongnbph22662/poly/duansotaydulich/CreateAccount.java
+++ app/src/main/java/cuongnbph22662/poly/duansotaydulich/CreateAccount.java
@@ -10,5 +10,6 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
-    }
+        //HUNG NOTE TEXT
+        }
 }
