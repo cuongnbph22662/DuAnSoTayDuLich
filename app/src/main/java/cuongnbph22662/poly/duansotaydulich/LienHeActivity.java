ee42f5ba4027bff439e0121b672b1425d28a5cb7
Hoangseven7 <tatranminhhoangseven@gmail.com>
2022-11-29 21:23:26 +0700

    day2

---
+++ app/src/main/java/cuongnbph22662/poly/duansotaydulich/LienHeActivity.java
@@ -0,0 +1,14 @@
+package cuongnbph22662.poly.duansotaydulich;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LienHeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lien_he);
+    }
+}
