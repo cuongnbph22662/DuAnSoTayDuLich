ffec808d69b2fefb652e1a29492033cca6edb1d9
thuynt29 <ntt2992003@gmail.com>
2022-11-30 10:54:32 +0700

    30/11 thuy xinh gai
    30/11 thuy xinh gai

--- app/src/main/java/cuongnbph22662/poly/duansotaydulich/ChiaSeActivity.java
+++ app/src/main/java/cuongnbph22662/poly/duansotaydulich/ChiaSeActivity.java
@@ -9,17 +9,11 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class ChiaSeActivity extends AppCompatActivity {
-    private ImageView img_anhchise;
-    private ImageButton btn_img;
-    private EditText edt_tieude;
-    private Button btn_dangtin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chia_se);
-        img_anhchise = findViewById(R.id.img_anhchiase);
-        btn_img = findViewById(R.id.btn_img);
-        edt_tieude = findViewById(R.id.edt_tieude);
-        btn_dangtin = findViewById(R.id.btn_dangtin);
+
     }
 }
