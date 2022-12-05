a4af219a2efa621c69fc8e0ade99bdebf475534f
hungvhph22652 <117882306+hungvhph22652@users.noreply.github.com>
2022-12-05 14:42:03 +0700

    comment

--- app/src/main/java/cuongnbph22662/poly/duansotaydulich/activity/DoiMatKhauActivity.java
+++ app/src/main/java/cuongnbph22662/poly/duansotaydulich/activity/DoiMatKhauActivity.java
@@ -30,49 +30,49 @@ import cuongnbph22662.poly.duansotaydulich.model.NguoiDung;
         nguoiDungDAO = new NguoiDungDAO(getApplicationContext());
         edRePass = findViewById(R.id.ed_XacNhanMKMoi);
         btnSave = findViewById(R.id.btnSave);
-//        btnSave.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                SharedPreferences preferences = getSharedPreferences("USER_FILE",Context.MODE_PRIVATE);
-//                String user = preferences.getString("taiKhoan","");
-//                if (validate()>0){
-//                    NguoiDung nguoiDung = nguoiDungDAO.getID(user);
-//                    nguoiDung.setMatKhau(edPass.getText().toString());
-//                    if (nguoiDungDAO.update(nguoiDung)>0){
-//                        Toast.makeText(getApplicationContext(),"Thay đôie mật khẩu thành công",Toast.LENGTH_SHORT).show();
-//                        edPassOld.setText("");
-//                        edPass.setText("");
-//                        edRePass.setText("");
-//                    }
-//                    else{
-//                        Toast.makeText(getApplicationContext(),"Thay đôie mật khẩu không thành công",Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//            }
-//        });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences = getSharedPreferences("USER_FILE",MODE_PRIVATE);
+                String user = preferences.getString("taiKhoan","");
+
+                if (validate()>0){
+                    NguoiDung nguoiDung = nguoiDungDAO.getID(user);
+                    nguoiDung.setMatKhau(edPass.getText().toString());
+                    if (nguoiDungDAO.update(nguoiDung)>0){
+                        Toast.makeText(getApplicationContext(),"Thay đôie mật khẩu thành công",Toast.LENGTH_SHORT).show();
+                        edPassOld.setText("");
+                        edPass.setText("");
+                        edRePass.setText("");
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(),"Thay đôie mật khẩu không thành công",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
 
     }
-//    public int validate(){
-//        int check = 1;
-//        if (edPassOld.getText().length()==0||edPass.getText().length()==0||edRePass.getText().length()==0){
-//            Toast.makeText(getApplicationContext(),"Bạn phải điền đủ thông tin",Toast.LENGTH_SHORT).show();
-//            check = -1;
-//        }
-//        else{
-//            SharedPreferences preferences = getSharedPreferences("USER_FILE",Context.MODE_PRIVATE);
-//            String passOld = preferences.getString("matKhau","");
-//            String pass = edPass.getText().toString();
-//            String repass = edRePass.getText().toString();
-//            if (!passOld.equals(edPassOld.getText().toString())){
-//                Toast.makeText(getApplicationContext(),"Mật khẩu cũ sai",Toast.LENGTH_SHORT).show();
-//                check = -1;
-//            }
-//            if (!pass.equals(repass)){
-//                Toast.makeText(getApplicationContext(),"Mật khẩu không trùng khớp",Toast.LENGTH_SHORT).show();
-//                check = -1;
-//
-//            }
-//        }
-//        return check;
-//    }
+    public int validate(){
+        int check = 1;
+        if (edPassOld.getText().length()==0||edPass.getText().length()==0||edRePass.getText().length()==0){
+            Toast.makeText(getApplicationContext(),"Bạn phải điền đủ thông tin",Toast.LENGTH_SHORT).show();
+            check = -1;
+        }
+        else{
+            SharedPreferences preferences = getSharedPreferences("USER_FILE",MODE_PRIVATE);
+            String passOld = preferences.getString("matKhau","");
+            String pass = edPass.getText().toString();
+            String repass = edRePass.getText().toString();
+            if (!passOld.equals(edPassOld.getText().toString())){
+                Toast.makeText(getApplicationContext(),"Mật khẩu cũ sai",Toast.LENGTH_SHORT).show();
+                check = -1;
+            }
+            if (!pass.equals(repass)){
+                Toast.makeText(getApplicationContext(),"Mật khẩu không trùng khớp",Toast.LENGTH_SHORT).show();
+                check = -1;
+            }
+        }
+        return check;
+    }
 }
